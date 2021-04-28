160ae8bed322bfcdc785d3df8523796eeacfe7d0
Teniola Etti <71016291+tenioe@users.noreply.github.com>
2021-04-28 17:17:04 +0100

    Merge pull request #23 from tenioe/t0/init - added 'gust'& test case methods

--- src/test/java/com/sparta/eng82/openweatherapi/framework/interfaces/dto/component/WindDTO.java
+++ src/test/java/com/sparta/eng82/openweatherapi/framework/interfaces/dto/component/WindDTO.java
@@ -4,7 +4,12 @@ import com.sparta.eng82.openweatherapi.framework.interfaces.DTOChecker;
 
 public interface WindDTO extends DTOChecker {
     Double getSpeed();
-
     Double getDegree();
+    Double getGust();
+
+    Boolean isSpeedBelowZero();
+    Boolean isGustBelowZero();
+    Boolean isDegreeBelowZero();
+
 
 }
