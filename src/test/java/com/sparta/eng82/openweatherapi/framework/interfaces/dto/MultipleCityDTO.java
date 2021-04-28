72facff26b799e52f587c211b6fdd0c2db9355a4
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-04-28 15:49:16 +0100

    added getters for multi city

--- src/test/java/com/sparta/eng82/openweatherapi/framework/interfaces/dto/MultipleCityDTO.java
+++ src/test/java/com/sparta/eng82/openweatherapi/framework/interfaces/dto/MultipleCityDTO.java
@@ -3,5 +3,12 @@ package com.sparta.eng82.openweatherapi.framework.interfaces.dto;
 import com.sparta.eng82.openweatherapi.framework.interfaces.DTOChecker;
 import com.sparta.eng82.openweatherapi.framework.interfaces.StatusCodeResponse;
 
+import java.util.ArrayList;
+
 public interface MultipleCityDTO extends DTOChecker, StatusCodeResponse {
+
+    ArrayList<CityDTO> getCities();
+
+    Integer getCount();
+    String getMessage();
 }
