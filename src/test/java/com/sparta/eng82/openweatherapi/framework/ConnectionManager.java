3285bffafd681cbe2125ebfc56142a072f22d99f
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-04-29 11:04:37 +0100

    Connection manager implemented into a 'static' class

---
+++ src/test/java/com/sparta/eng82/openweatherapi/framework/ConnectionManager.java
@@ -0,0 +1,54 @@
+package com.sparta.eng82.openweatherapi.framework;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConnectionManager {
+
+    private static final String BASE_URL = "api.openweathermap.org/data/2.5/";
+    private static final String API_KEY_ENDPOINT = "&appid={API key}";
+
+    private static String apiKey;
+
+    static {
+        try (InputStream input = new FileInputStream("src/test/resources/application.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            apiKey = prop.getProperty("api.key");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getConnection(EndPoint endPoint, String... params) {
+        return BASE_URL + buildConnection(endPoint.getUrl(), params) + apiKeyEndPoint();
+    }
+
+    private static String buildConnection(String by, String... params) {
+        StringBuilder builder = new StringBuilder(by);
+        int openBracket = builder.indexOf("{");
+        int closeBracket;
+        int paramCount = 0;
+        while (openBracket != -1) {
+            closeBracket = builder.indexOf("}");
+            if (paramCount < params.length) {
+                builder.replace(openBracket, closeBracket, params[paramCount]);
+            } else {
+                builder.delete(openBracket - 1, closeBracket);
+            }
+            paramCount++;
+            openBracket = builder.indexOf("{");
+        }
+        return builder.toString();
+    }
+
+    private static String apiKeyEndPoint() {
+        StringBuilder builder = new StringBuilder(API_KEY_ENDPOINT);
+        int startIndex = builder.indexOf("{");
+        int endIndex = builder.indexOf("}");
+        return builder.replace(startIndex, endIndex, apiKey).toString();
+    }
+}
