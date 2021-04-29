e79c4de582f9bff1d33fe768dffa77fc61fa5248
alexsikorski <alexsikorski22@gmail.com>
2021-04-29 14:32:09 +0100

    moved csv file reading into static block

--- src/test/java/com/sparta/eng82/openweatherapi/framework/dto/component/WeatherDTOImpl.java
+++ src/test/java/com/sparta/eng82/openweatherapi/framework/dto/component/WeatherDTOImpl.java
@@ -12,12 +12,23 @@ import java.util.List;
 
 public class WeatherDTOImpl implements WeatherDTO {
 
+    private static final List<List<String>> weatherConditions;
+    private static final List<List<String>> weatherIcons;
+
+    static {
+        weatherConditions = readCsvFileIntoArray("src/test/resources/weather_conditions.csv");
+        weatherIcons = readCsvFileIntoArray("src/test/resources/weather_icons.csv");
+    }
+
     @JsonProperty("id")
     private Integer weatherId;
+
     @JsonProperty("main")
     private String weatherGroup;
+
     @JsonProperty("description")
     private String weatherDescription;
+
     @JsonProperty("icon")
     private String weatherIcon;
 
@@ -46,24 +57,24 @@ public class WeatherDTOImpl implements WeatherDTO {
 
     @Override
     public boolean checkWeatherIdMatchesGroup() {
-        return matchTwoValues("src/test/resources/weather_conditions.csv", weatherId.toString(), weatherGroup, 1);
+        return matchTwoValues(weatherId.toString(), weatherGroup, 1);
     }
 
     @Override
     public boolean checkWeatherIdMatchesDescription() {
-        return matchTwoValues("src/test/resources/weather_conditions.csv", weatherId.toString(), weatherDescription, 2);
+        return matchTwoValues(weatherId.toString(), weatherDescription, 2);
     }
 
     @Override
     public boolean checkWeatherIconMatchesDescription() {
-        return matchTwoValuesTwoCsv("src/test/resources/weather_icons.csv", "src/test/resources/weather_conditions.csv", weatherIcon, weatherDescription);
+        return matchTwoValuesTwoCsv(weatherIcon, weatherDescription);
     }
 
-    private boolean matchTwoValuesTwoCsv(String firstFilePath, String secondFilePath, String findFirst, String matchSecond) {
-        for (List<String> firstRow : readCsvFileIntoArray(firstFilePath)) {
+    private boolean matchTwoValuesTwoCsv(String findFirst, String matchSecond) {
+        for (List<String> firstRow : weatherIcons) {
             for (String firstEntry : firstRow) {
                 if (firstEntry.equals(findFirst)) {
-                    for (List<String> secondRow : readCsvFileIntoArray(secondFilePath)) {
+                    for (List<String> secondRow : weatherConditions) {
                         for (String secondEntry : secondRow) {
                             if (secondEntry.toLowerCase().equals(matchSecond)) {
                                 if (secondRow.get(3).equals(firstEntry)) {
@@ -78,8 +89,8 @@ public class WeatherDTOImpl implements WeatherDTO {
         return false;
     }
 
-    private boolean matchTwoValues(String filePath, String findValue, String matchValue, int index) {
-        for (List<String> row : readCsvFileIntoArray(filePath)) {
+    private boolean matchTwoValues(String findValue, String matchValue, int index) {
+        for (List<String> row : weatherConditions) {
             for (String entry : row) {
                 if (entry.equals(findValue)) {
                     if (row.get(index).equals(matchValue)) {
@@ -91,7 +102,7 @@ public class WeatherDTOImpl implements WeatherDTO {
         return false;
     }
 
-    private List<List<String>> readCsvFileIntoArray(String filePath) {
+    private static List<List<String>> readCsvFileIntoArray(String filePath) {
         List<List<String>> temp = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
