ea63108f45e78a4a4573fceb479c8ad204533fe1
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-04-29 13:06:39 +0100

    moved responsibility of generating the url to the EndPoint enum

--- src/test/java/com/sparta/eng82/openweatherapi/framework/ConnectionManager.java
+++ src/test/java/com/sparta/eng82/openweatherapi/framework/ConnectionManager.java
@@ -7,9 +7,6 @@ import java.util.Properties;
 
 public class ConnectionManager {
 
-    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
-    private static final String API_KEY_ENDPOINT = "&appid={API key}";
-
     private static String apiKey;
 
     static {
@@ -23,32 +20,9 @@ public class ConnectionManager {
         }
     }
 
-    public static String getConnection(EndPoint endPoint, String... params) {
-        return BASE_URL + buildConnection(endPoint.getUrl(), params) + apiKeyEndPoint();
-    }
-
-    private static String buildConnection(String by, String... params) {
-        StringBuilder builder = new StringBuilder(by);
-        int openBracket = builder.indexOf("{");
-        int closeBracket;
-        int paramCount = 0;
-        while (openBracket != -1) {
-            closeBracket = builder.indexOf("}");
-            if (paramCount < params.length) {
-                builder.replace(openBracket, closeBracket + 1, params[paramCount]);
-            } else {
-                builder.delete(openBracket - 1, closeBracket + 1);
-            }
-            paramCount++;
-            openBracket = builder.indexOf("{");
-        }
-        return builder.toString();
-    }
-
-    private static String apiKeyEndPoint() {
-        StringBuilder builder = new StringBuilder(API_KEY_ENDPOINT);
-        int startIndex = builder.indexOf("{");
-        int endIndex = builder.indexOf("}");
-        return builder.replace(startIndex, endIndex + 1, apiKey).toString();
+    public static EndPoint getConnection(EndPoint endPoint, String... params) {
+        endPoint.setParams(params);
+        endPoint.setAPIKey(apiKey);
+        return endPoint.buildEndPoint();
     }
 }
