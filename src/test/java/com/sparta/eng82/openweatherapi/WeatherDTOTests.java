247095ea6d5011dd54a1bcd86723e8af84ba2c6d
alexsikorski <alexsikorski22@gmail.com>
2021-04-29 15:59:37 +0100

    Refactored CityDTO to support tests used in WeatherDTOTests for further abstraction. Added tests to WeatherDTOTests.

--- src/test/java/com/sparta/eng82/openweatherapi/WeatherDTOTests.java
+++ src/test/java/com/sparta/eng82/openweatherapi/WeatherDTOTests.java
@@ -4,18 +4,10 @@ import com.sparta.eng82.openweatherapi.framework.ConnectionManager;
 import com.sparta.eng82.openweatherapi.framework.EndPoint;
 import com.sparta.eng82.openweatherapi.framework.Injector;
 import com.sparta.eng82.openweatherapi.framework.interfaces.dto.CityDTO;
-import com.sparta.eng82.openweatherapi.framework.interfaces.dto.component.WeatherDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
 
 public class WeatherDTOTests {
 
@@ -29,33 +21,26 @@ public class WeatherDTOTests {
     }
 
     @Test
-    @DisplayName("Check weather id matches group")
-    void checkWeatherIdMatchesGroup() {
-        Assertions.assertTrue(cityDTO.getWeatherDTO().get(0).checkWeatherIdMatchesGroup());
+    @DisplayName("Check all weather ids match groups")
+    void checkAllWeatherIdsMatchGroups() {
+        Assertions.assertTrue(cityDTO.doWeatherIdsMatchGroup());
     }
 
     @Test
-    @DisplayName("Check weather icon matches description")
-    void checkWeatherIconMatchesDescription() {
-        Assertions.assertTrue(cityDTO.getWeatherDTO().get(0).checkWeatherIconMatchesDescription());
+    @DisplayName("Check all weather icon ids match descriptions")
+    void checkAllWeatherIconIdsMatchDescriptions() {
+        Assertions.assertTrue(cityDTO.doWeatherIconIdsMatchDescription());
     }
 
     @Test
-    @DisplayName("Check weather id matches description")
-    void checkWeatherIdMatchesDescription() {
-        Assertions.assertTrue(cityDTO.getWeatherDTO().get(0).checkWeatherIdMatchesDescription());
+    @DisplayName("Check all weather ids match descriptions")
+    void checkAllWeatherIdsMatchDescriptions() {
+        Assertions.assertTrue(cityDTO.doWeatherIdsMatchDescription());
     }
 
     @Test
-    @DisplayName("Check weather id is not null")
-    void checkWeatherIdIsNotNull() {
-        Assertions.assertNotNull(cityDTO.getWeatherDTO().get(0));
-
-    }
-
-    @Test
-    @DisplayName("Check all id are not null")
-    void checkAllIdAreNotNull() {
-       Assertions.assertTrue(cityDTO.areWeatherIdsNotNull());
+    @DisplayName("Check all weather ids are not null")
+    void checkAllWeatherIdsAreNotNull() {
+        Assertions.assertTrue(cityDTO.areWeatherIdsNotNull());
     }
 }
